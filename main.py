a4fc9e6da99ec92d8ddea3d7d66f946b85c7c3fe
Alona Konstantinova <alona@ruc.dk>
2024-02-14 17:11:05 +0100

    data commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import pandas as pd
+
+data = pd.read_csv('diamonds.csv')
+print(data.head())
+
+#Are there issues with outliers in the data?
+
+#Do the attributes appear to be normal distributed?
+
+#Are variables correlated
