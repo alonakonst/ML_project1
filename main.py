21cce247b73c581aa34f3517b6d2e7da98a7b618
Alona Konstantinova <alona@ruc.dk>
2024-02-16 16:13:01 +0100

    outliers, distribution, correlation

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import pandas as pd
-
-data = pd.read_csv('diamonds.csv')
-print(data.head())
-
-#Are there issues with outliers in the data?
-
-#Do the attributes appear to be normal distributed?
-
-#Are variables correlated
+'''
+Here we will put all the code together
+'''
